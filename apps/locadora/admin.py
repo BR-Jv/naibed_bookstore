7d4dc0e0e8a4a10349fb771ed73bb6fa9aad704b
João.Victor <jv.alves193@gmail.com>
2025-06-28 21:31:52 -0300

    Adicionado models Cliente e Pedido

--- apps/locadora/admin.py
+++ apps/locadora/admin.py
@@ -1,9 +1,25 @@
 from django.contrib import admin
 
-from .models import Livro
+from .models import *
 
 @admin.register(Livro)
 class LivroAdmin(admin.ModelAdmin):
     list_display = ["titulo", "preco", "ano"]
     
 
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):    
+    list_display = ('__str__', 'email', 'ativo')
+    search_fields = ('nome', 'sobrenome')
+    list_filter = ('ativo',)
+
+@admin.register(Pedido)
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'cliente', 'status')
+    search_fields = (
+        'cliente__nome',
+        'cliente__sobrenome',
+        'cliente__email',
+    )
+    list_filter = ('status',)
+    date_hierarchy = 'created'
--- apps/locadora/models.py
+++ apps/locadora/models.py
@@ -13,3 +13,55 @@ class Livro(models.Model):
     def __str__(self):
         return self.titulo
     
+class Cliente(models.Model):
+    nome = models.CharField()
+    sobrenome = models.CharField()
+    email = models.EmailField()
+    ativo = models.BooleanField()
+
+    class Meta: 
+        ordering = ['nome']
+        verbose_name = 'cliente'
+        verbose_name_plural = 'clientes'
+
+    @property
+    def nome_completo(self):
+        return f'{self.nome} {self.sobrenome or ""}'.strip()
+    
+
+    def __str__(self):
+        return self.nome_completo
+
+
+
+STATUS = (
+    ('p','Pendente'),
+    ('a','Aprovado'),
+    ('c','Cancelado'),
+)
+
+
+class Pedido(models.Model):
+    status = models.CharField(max_length=1, choices=STATUS, default='p')
+    
+    cliente = models.ForeignKey(
+        Cliente, 
+        on_delete=models.SET_NULL,
+        related_name='pedidos',
+        null=True,
+        blank=True
+        )
+    
+    created = models.DateTimeField('criado em', auto_now_add=True, auto_now=False)
+
+    class Meta: 
+        ordering = ['-pk']
+        verbose_name = 'ordem de compra'
+        verbose_name_plural = 'ordens de compra'
+    
+    def __str__(self):
+
+        if self.cliente:
+            return f'{str(self.pk).zfill(3)}-{self.cliente}'
+
+        return f'{str(self.pk).zfill(3)}'
