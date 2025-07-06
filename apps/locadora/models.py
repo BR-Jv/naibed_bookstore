945d557821b5fb1537c815966287edeb3bb5fe60
João.Victor <jv.alves193@gmail.com>
2025-07-05 22:35:48 -0300

    Iniciando analise de requisitos e modificando as models

--- apps/locadora/models.py
+++ apps/locadora/models.py
@@ -51,6 +51,8 @@ class Pedido(models.Model):
         blank=True
         )
     
+    # Quantidade 
+
     created = models.DateTimeField('criado em', auto_now_add=True, auto_now=False)
 
     class Meta: 
@@ -60,39 +62,4 @@ class Pedido(models.Model):
     
     def __str__(self):
 
-        if self.cliente:
-            return f'{str(self.pk).zfill(3)}-{self.cliente}'
-
-        return f'{str(self.pk).zfill(3)}'
-
-
-METODO_PAGAMENTO = (
-    ("di", "dinheiro"),
-    ("cr", "crédito"),
-    ("de", "débito"),
-    ("pi", "Pix"),
-)
-
-
-class Venda(models.Model):
-    pedido = models.OneToOneField(
-        Pedido,
-        on_delete=models.CASCADE,
-        verbose_name='ordem de compra'
-    )
-    status = models.BooleanField('pago', default=False)
-    data_pagamento = models.DateTimeField('data de pagamento', null=True, blank=True)
-    metodo = models.CharField('forma de pagamento', max_length=3, choices=METODO_PAGAMENTO)
-    entrega = models.PositiveSmallIntegerField('prazo de entrega', default=15)
-    created = models.DateTimeField('criado em', auto_now_add=True, auto_now=False)
-
-    class Meta:
-        ordering = ('-pk',)
-        verbose_name = 'venda'
-        verbose_name_plural = 'vendas'
-
-    def __str__(self):
-        if self.pedido:
-            return f'{str(self.pk).zfill(3)}-{self.pedido}'
-
-        return f'{str(self.pk).zfill(3)}'
+        pass
